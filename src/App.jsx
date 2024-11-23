6a475a71b8cc1b33e56f64cae550bfcd2386acd0
Xavi Enriquez <113799913+XavierEnriquez@users.noreply.github.com>
2024-11-23 17:46:19 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,29 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./ui/Home";
+import Menu from "./features/menu/Menu";
+import { loader as menuLoader } from "./features/menu/menuLoader";
+import Cart from "./features/cart/Cart";
+import Order from "./features/order/Order";
+import CreateOrder from "./features/order/CreateOrder";
+import AppLayout from "./ui/AppLayout";
+import Error from "./ui/Error";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    errorElement: <Error />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/menu", element: <Menu />, loader: menuLoader },
+      { path: "/cart", element: <Cart /> },
+      { path: "/order/:orderId", element: <Order /> },
+      { path: "/order/new", element: <CreateOrder /> },
+    ],
+  },
+]);
+
 function App() {
-  return <div>Hello Vite!</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
