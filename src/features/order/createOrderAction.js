00f50c364462965ab68cc4d6c3caef53e2a67588
Xavi Enriquez <113799913+XavierEnriquez@users.noreply.github.com>
2024-11-24 11:27:22 -0600

    Create createOrderAction.js

---
+++ src/features/order/createOrderAction.js
@@ -0,0 +1,29 @@
+import { redirect } from "react-router-dom";
+import { createOrder } from "../../services/apiRestaurant";
+
+// https://uibakery.io/regex-library/phone-number
+const isValidPhone = (str) =>
+  /^\+?\d{1,3}?[-.\s]?\(?\d{1,2}?\)?[-.\s]?\d{1,3}[-.\s]?\d{1,3}[-.\s]?\d{1,5}?[-.\s]?\d{1,6}[-.\s]?\d{1,4}[-.\s]?\d{1,8}[-.\s]?\d{1,9}[-.\s]?\d{1,9}$/.test(
+    str
+  );
+
+export async function action({ request }) {
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+
+  const order = {
+    ...data,
+    cart: JSON.parse(data.cart),
+    priority: data.priority === "on",
+  };
+
+  const errors = {};
+  if (!isValidPhone(order.phone))
+    errors.phone = "Please enter a valid phone number (including area code)";
+
+  if (Object.keys(errors).length > 0) return errors;
+
+  const newOrder = await createOrder(order);
+
+  return redirect(`/order/${newOrder.id}`);
+}
