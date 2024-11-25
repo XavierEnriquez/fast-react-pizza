eddee47b39850cdf60d6535a3577024bab58eb56
Xavi Enriquez <113799913+XavierEnriquez@users.noreply.github.com>
2024-11-24 22:50:04 -0600

    Update Header.jsx

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,11 +1,15 @@
 import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder";
+import UserName from "../features/user/UserName";
 
 function Header() {
   return (
-    <header>
-      <Link to="/">Fast React Pizza Co.</Link>
+    <header className="flex flex-col gap-2 border-b border-stone-200 bg-yellow-500 px-8 py-4 text-center font-semibold uppercase sm:flex-row sm:justify-between">
+      <Link to="/" className="tracking-[.25rem]">
+        Fast React Pizza Co.
+      </Link>
       <SearchOrder />
+      <UserName />
     </header>
   );
 }
