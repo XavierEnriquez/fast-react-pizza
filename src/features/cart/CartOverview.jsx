1166b10b11b34224f1980103186a12e533205cac
Xavi Enriquez <113799913+XavierEnriquez@users.noreply.github.com>
2024-11-24 22:50:24 -0600

    Update CartOverview.jsx

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -2,12 +2,14 @@ import { Link } from "react-router-dom";
 
 function CartOverview() {
   return (
-    <div>
-      <p>
+    <div className="flex flex-wrap justify-between gap-4 bg-stone-800 px-8 py-4 uppercase text-stone-200">
+      <p className="space-x-4 text-nowrap font-semibold text-stone-300">
         <span>23 pizzas</span>
         <span>$23.45</span>
       </p>
-      <Link to="/cart">Open cart &rarr;</Link>
+      <Link to="/cart" className="text-nowrap">
+        Open cart &rarr;
+      </Link>
     </div>
   );
 }
