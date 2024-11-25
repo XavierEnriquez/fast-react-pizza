c571f813e806a0b1a5c0754b00e3d44f53479008
Xavi Enriquez <113799913+XavierEnriquez@users.noreply.github.com>
2024-11-25 13:31:48 -0600

    Update CreateOrder.jsx

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,5 +1,6 @@
 // import { useState } from "react";
 import { Form, useActionData, useNavigation } from "react-router-dom";
+import Button from "../../ui/Button";
 
 const fakeCart = [
   {
@@ -73,12 +74,9 @@ function CreateOrder() {
         </div>
         <input type="hidden" name="cart" value={JSON.stringify(cart)} />
         <div>
-          <button
-            disabled={isSubmitting}
-            className="inline-block rounded-md bg-yellow-400 px-4 py-2 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-400 focus:ring-offset-2 disabled:cursor-not-allowed"
-          >
+          <Button type="primary" disabled={isSubmitting}>
             {isSubmitting ? "Placing order..." : "Order now"}
-          </button>
+          </Button>
         </div>
       </Form>
     </div>
