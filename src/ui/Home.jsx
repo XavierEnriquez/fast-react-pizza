d85bd3b758b86a8da2da8584414a365229bc1a56
Xavi Enriquez <113799913+XavierEnriquez@users.noreply.github.com>
2024-11-24 22:50:00 -0600

    Update Home.jsx

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,11 +1,16 @@
+import CreateUser from "../features/user/CreateUser";
+
 function Home() {
   return (
-    <div>
-      <h1>
+    <div className="text-center">
+      <h1 className="mb-10 text-3xl font-semibold">
         The best pizza.
         <br />
-        Straight out of the oven, straight to you.
+        <span className="font-bold text-yellow-500">
+          Hot out of the oven, straight to you.
+        </span>
       </h1>
+      <CreateUser />
     </div>
   );
 }
