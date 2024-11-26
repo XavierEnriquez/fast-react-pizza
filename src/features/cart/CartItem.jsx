5e0eb265d51a5e421f9689d3eb5cc0012c1c5c3e
Xavi Enriquez <113799913+XavierEnriquez@users.noreply.github.com>
2024-11-26 09:05:21 -0600

    Update CartItem.jsx

--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -5,14 +5,14 @@ function CartItem({ item }) {
   const { pizzaId, name, quantity, totalPrice } = item;
 
   return (
-    <li className="py-4 flex justify-between min-w-60 gap-8 items-center">
-      <div className="flex justify-between flex-grow flex-wrap  " >
-      <p className="mb-1 min-w-60">
-        {quantity}&times; {name}
-      </p>
+    <li className="flex min-w-60 items-center justify-between gap-8 py-4">
+      <div className="flex flex-grow flex-wrap justify-between">
+        <p className="mb-1 min-w-60">
+          {quantity}&times; {name}
+        </p>
         <p className="text-sm font-bold">{formatCurrency(totalPrice)}</p>
       </div>
-        <Button className='small'>Delete</Button>
+      <Button className="small">Delete</Button>
     </li>
   );
 }
