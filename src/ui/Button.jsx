b43f8aa9dd5ca6f6a346cb075bf8b1439eb4448f
Xavi Enriquez <113799913+XavierEnriquez@users.noreply.github.com>
2024-11-25 23:58:04 -0600

    Update Button.jsx

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,15 +1,14 @@
 import { Link } from "react-router-dom";
 
 function Button({ type, className, to, disabled, children }) {
-  const base = 
-    "inline-block rounded-md bg-yellow-400 font-semibold uppercase tracking-wide text-stone-800 transition-all duration-300 hover:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-400 focus:ring-offset-2 disabled:cursor-not-allowed";
+  const base = "inline-block rounded-md bg-yellow-400 font-semibold uppercase tracking-wide text-stone-800 transition-all duration-300 hover:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-400 focus:ring-offset-2 disabled:cursor-not-allowed"
+    ;
 
   const styles = {
     primary: `${base} px-4 py-3 sm:px-5 sm:py-4`,
     small: `${base} px-3 py-2 text-sm sm:px-3 sm:px-4`,
-    link: 
-      "text-nowrap text-sm font-semibold text-blue-500 hover:text-blue-600",
-    link_cart: ("text-nowrap font-semibold hover:text-white/60"),
+    link: "text-nowrap text-sm px-4 py-3 font-semibold text-blue-500 hover:text-blue-600",
+    link_cart: "text-nowrap px-4 py-3 font-semibold hover:text-white/60",
   };
 
   if (type === "link")
