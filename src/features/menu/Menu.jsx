df322406926606ff19136d6536c66b2527cb86c3
Xavi Enriquez <113799913+XavierEnriquez@users.noreply.github.com>
2024-11-25 21:17:29 -0600

    Update Menu.jsx

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -4,7 +4,7 @@ import MenuItem from "../menu/MenuItem";
 function Menu() {
   const menu = useLoaderData();
   return (
-    <ul>
+    <ul className="flex flex-wrap divide-y divide-stone-300 px-2 md:gap-x-12 lg:justify-between">
       {menu.map((pizza) => (
         <MenuItem pizza={pizza} key={pizza.id} />
       ))}
