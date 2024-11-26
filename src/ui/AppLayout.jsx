179d576ad63caa97425459e7417fd6d0d49b47cd
Xavi Enriquez <113799913+XavierEnriquez@users.noreply.github.com>
2024-11-26 16:31:43 -0600

    Update AppLayout.jsx

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -10,7 +10,7 @@ function AppLayout() {
     <div className="grid h-screen grid-rows-[auto_1fr_auto]">
       {isLoading && <LoaderIndicator />}
       <Header />
-      <main className="overflow-auto px-4 pb-4 pt-8 md:px-8 md:pb-8 md:pt-16">
+      <main className="overflow-auto px-4 pb-4 pt-8 md:px-8 md:pb-8 md:pt-12">
         <Outlet />
       </main>
       <CartOverview />
