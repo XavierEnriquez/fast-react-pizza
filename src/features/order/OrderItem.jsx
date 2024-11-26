a45ecfad08319b4aeba5548ce70213bb61c281df
Xavi Enriquez <113799913+XavierEnriquez@users.noreply.github.com>
2024-11-26 16:32:36 -0600

    Update OrderItem.jsx

--- src/features/order/OrderItem.jsx
+++ src/features/order/OrderItem.jsx
@@ -1,15 +1,15 @@
-import formatCurrency from "../../utils/helpers";
+import { formatCurrency } from "../../utils/helpers";
 
 function OrderItem({ item, isLoadingIngredients, ingredients }) {
   const { quantity, name, totalPrice } = item;
 
   return (
-    <li>
-      <div>
-        <p>
-          <span>{quantity}&times;</span> {name}
+    <li className="py-3">
+      <div className="flex flex-wrap items-center justify-between">
+        <p className="basis-60">
+          <span className="font-semibold">{quantity}&times;</span> {name}
         </p>
-        <p>{formatCurrency(totalPrice)}</p>
+        <p className="font-semibold">{formatCurrency(totalPrice)}</p>
       </div>
     </li>
   );
