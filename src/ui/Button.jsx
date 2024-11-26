1ea4a6c6367edffb616e423c0fecb8425c9b2a67
Xavi Enriquez <113799913+XavierEnriquez@users.noreply.github.com>
2024-11-26 16:31:37 -0600

    Update Button.jsx

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,21 +1,21 @@
 import { Link } from "react-router-dom";
 import clsx from "clsx";
 
-/**Using clsx to sort Tailwind css classes in text strings passed on into a function. clsx is a (239B) utility for constructing className strings conditionally by Luke Edwards. https://github.com/lukeed/clsx  */
+/*Using clsx to sort Tailwind css classes in text strings passed on into a function. clsx is a (239B) utility for constructing className strings conditionally by Luke Edwards. https://github.com/lukeed/clsx  */
 
 function Button({ type, className, to, disabled, children }) {
   const base = clsx(
-    "inline-block rounded-xl font-semibold uppercase tracking-wide transition-all duration-300 focus:outline-none focus:ring focus:ring-offset-2 disabled:cursor-not-allowed",
+    "inline-block rounded-xl font-bold uppercase tracking-wide transition-all duration-300 focus:outline-none focus:ring focus:ring-offset-2 disabled:cursor-not-allowed",
   );
 
   const primary = clsx(
-    "bg-yellow-400 px-4 py-3 text-stone-800 hover:bg-yellow-300 focus:ring-yellow-300 sm:px-5 sm:py-4",
+    "bg-yellow-400 px-4 py-3 text-stone-700 hover:bg-yellow-300 focus:ring-yellow-300 sm:px-5 sm:py-4",
   );
   const secondary = clsx(
     "border-2 border-stone-300 px-4 py-3 text-stone-400 hover:bg-stone-300 hover:text-stone-800 focus:ring-stone-300 sm:px-5 sm:py-4",
   );
   const small = clsx(
-    "bg-yellow-400 px-3 py-2 text-sm text-stone-800 hover:bg-yellow-300 focus:ring-yellow-300 sm:px-4 sm:py-3",
+    "bg-yellow-400 px-3 py-2 text-sm text-stone-700 hover:bg-yellow-300 focus:ring-yellow-300 sm:px-4 sm:py-3",
   );
   const link = clsx(
     "text-nowrap px-4 py-3 text-sm font-semibold text-blue-500 hover:text-blue-600",
