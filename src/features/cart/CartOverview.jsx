b45f2c07578bbfc818632e43874da830554027f1
Xavi Enriquez <113799913+XavierEnriquez@users.noreply.github.com>
2024-11-26 18:47:06 -0600

    Update CartOverview.jsx

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -1,14 +1,23 @@
+import { useSelector } from "react-redux";
+
+import { formatCurrency } from "../../utils/helpers";
+import { getCartQuantity, getCartTotal } from "./cartSlice";
 import Button from "../../ui/Button";
 
 function CartOverview() {
+  const cartQuantity = useSelector(getCartQuantity);
+  const cartTotal = useSelector(getCartTotal);
+
+  if (!cartQuantity) return null;
+
   return (
-    <div className="flex flex-wrap justify-between items-center gap-4 bg-stone-800 px-8 py-4 uppercase text-stone-200">
+    <div className="flex flex-wrap items-center justify-between gap-4 bg-stone-800 px-8 py-4 uppercase text-stone-200">
       <p className="space-x-4 text-nowrap font-semibold text-stone-300">
-        <span>23 pizzas</span>
-        <span>$23.45</span>
+        <span>{cartQuantity} pizzas</span>
+        <span> {formatCurrency(cartTotal)}</span>
       </p>
       <Button type="link" to="/cart" className="link_cart">
-        Open cart &rarr;
+        Place your order &rarr;
       </Button>
     </div>
   );
