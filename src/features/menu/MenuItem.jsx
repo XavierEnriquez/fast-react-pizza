1978a52b5b2dbf79489b8e34311595bb258645a1
Xavi Enriquez <113799913+XavierEnriquez@users.noreply.github.com>
2024-11-26 18:46:13 -0600

    Update MenuItem.jsx

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,8 +1,22 @@
 import { formatCurrency } from "../../utils/helpers";
 import Button from "../../ui/Button";
+import { useDispatch } from "react-redux";
+import { addItem } from "../cart/cartSlice";
 
 function MenuItem({ pizza }) {
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+  const dispatch = useDispatch();
+
+  function handleAddToCart() {
+    const newItem = {
+      pizzaId: id,
+      name: name,
+      quantity: 1,
+      unitPrice: unitPrice,
+      totalPrice: unitPrice * 1,
+    };
+    dispatch(addItem(newItem));
+  }
 
   return (
     <li className="max-w-2/5 flex flex-grow gap-4 py-2 md:w-2/5 xl:w-1/4 xl:py-4">
@@ -28,7 +42,11 @@ function MenuItem({ pizza }) {
               </p>
             )}
           </div>
-          <Button className="small">Add to cart</Button>
+          {!soldOut && (
+            <Button onClick={handleAddToCart} className="small">
+              Add to cart
+            </Button>
+          )}
         </div>
       </div>
     </li>
