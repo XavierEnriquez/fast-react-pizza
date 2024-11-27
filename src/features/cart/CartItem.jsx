d740f3a996f3c0d61e9e234b47aa55dce6c41806
Xavi Enriquez <113799913+XavierEnriquez@users.noreply.github.com>
2024-11-26 18:47:13 -0600

    Update CartItem.jsx

--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -1,18 +1,18 @@
-import Button from "../../ui/Button";
 import { formatCurrency } from "../../utils/helpers";
+import DeleteItem from "./DeleteItem";
 
 function CartItem({ item }) {
   const { pizzaId, name, quantity, totalPrice } = item;
 
   return (
-    <li className="flex min-w-60 items-center justify-between gap-8 py-4">
+    <li className="flex min-w-60 items-center justify-between gap-8 py-2">
       <div className="flex flex-grow flex-wrap justify-between">
         <p className="mb-1 min-w-60">
           {quantity}&times; {name}
         </p>
         <p className="text-sm font-bold">{formatCurrency(totalPrice)}</p>
       </div>
-      <Button className="small">Delete</Button>
+      <DeleteItem pizzaId={pizzaId} />
     </li>
   );
 }
