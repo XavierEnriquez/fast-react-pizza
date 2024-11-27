91f5216db7e84d74155dfff8be362882f82a60aa
Xavi Enriquez <113799913+XavierEnriquez@users.noreply.github.com>
2024-11-26 18:46:18 -0600

    Update EmptyCart.jsx

--- src/features/cart/EmptyCart.jsx
+++ src/features/cart/EmptyCart.jsx
@@ -1,11 +1,15 @@
-import { Link } from 'react-router-dom';
+import Button from "../../ui/Button";
 
 function EmptyCart() {
   return (
-    <div>
-      <Link to="/menu">&larr; Back to menu</Link>
+    <div className="m-auto mt-[5vmin] w-1/2 space-y-4">
+      <Button type="link" to="/menu" className="link">
+        &larr; Back to menu
+      </Button>
 
-      <p>Your cart is still empty. Start adding some pizzas :)</p>
+      <p className="text-lg font-semibold">
+        Your cart is empty. Visit our menu to order some pizzas :)
+      </p>
     </div>
   );
 }
