220c74b26dc65f6beeb38fc961baea5f38f0b31c
Xavi Enriquez <113799913+XavierEnriquez@users.noreply.github.com>
2024-11-27 11:05:44 -0600

    Update CartItem.jsx

--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -7,7 +7,7 @@ function CartItem({ item }) {
   return (
     <li className="flex min-w-60 items-center justify-between gap-8 py-2">
       <div className="flex flex-grow flex-wrap justify-between">
-        <p className="mb-1 min-w-60">
+        <p className="mb-1 min-w-60 font-medium">
           {quantity}&times; {name}
         </p>
         <p className="text-sm font-bold">{formatCurrency(totalPrice)}</p>
