68e5a561815e9bd5b2405f697fda9c6f649b9044
Xavi Enriquez <113799913+XavierEnriquez@users.noreply.github.com>
2024-11-27 11:05:11 -0600

    Update Button.jsx

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import clsx from "clsx";
 
-/*Using clsx to sort Tailwind css classes in text strings passed on into a function. clsx is a (239B) utility for constructing className strings conditionally by Luke Edwards. https://github.com/lukeed/clsx  */
+/*Using clsx to sort Tailwind css classes. clsx is a (239B) utility for constructing className strings conditionally by Luke Edwards. https://github.com/lukeed/clsx  */
 
 function Button({ type, className, to, disabled, children, onClick }) {
   const base = clsx(
@@ -15,7 +15,13 @@ function Button({ type, className, to, disabled, children, onClick }) {
     "border-2 border-stone-300 px-4 py-3 text-stone-400 hover:bg-stone-300 hover:text-stone-800 focus:ring-stone-300 sm:px-5 sm:py-4",
   );
   const small = clsx(
-    "bg-yellow-400 px-3 py-2 text-sm text-stone-700 hover:bg-yellow-300 focus:ring-yellow-300 sm:px-4 sm:py-3",
+    "bg-yellow-400 px-3 py-2 text-xs text-stone-700 hover:bg-yellow-300 focus:ring-yellow-300 sm:px-4 sm:py-3",
+  );
+  const round = clsx(
+    "inline-block rounded-full bg-yellow-400 p-2 text-xs font-bold uppercase text-stone-700 transition-all duration-300 hover:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed sm:px-3",
+  );
+  const isDelete = clsx(
+    "border-2 border-red-300 px-2 py-1 text-xs text-red-400 hover:border-red-400 hover:bg-red-400 hover:text-red-50 focus:ring-yellow-300 sm:px-3 sm:py-2",
   );
   const link = clsx(
     "text-nowrap p-1 text-sm font-semibold text-blue-500 hover:text-blue-600",
@@ -28,6 +34,8 @@ function Button({ type, className, to, disabled, children, onClick }) {
     primary: `${base} ${primary}`,
     secondary: `${base} ${secondary}`,
     small: `${base} ${small}`,
+    round: `${round}`,
+    isDelete: `${base} ${isDelete}`,
     link: `${link}`,
     link_cart: `${link_cart}`,
   };
