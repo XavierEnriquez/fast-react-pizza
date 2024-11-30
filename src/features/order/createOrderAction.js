5f405127d0cd244e70a1a7afb92f104c2c1ffe81
Xavi Enriquez <113799913+XavierEnriquez@users.noreply.github.com>
2024-11-29 23:54:47 -0600

    Update createOrderAction.js

--- src/features/order/createOrderAction.js
+++ src/features/order/createOrderAction.js
@@ -1,10 +1,14 @@
 import { redirect } from "react-router-dom";
+
 import { createOrder } from "../../services/apiRestaurant";
+import { clearCart } from "../cart/cartSlice";
+
+import store from "../../store";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
   /^\+?\d{1,3}?[-.\s]?\(?\d{1,2}?\)?[-.\s]?\d{1,3}[-.\s]?\d{1,3}[-.\s]?\d{1,5}?[-.\s]?\d{1,6}[-.\s]?\d{1,4}[-.\s]?\d{1,8}[-.\s]?\d{1,9}[-.\s]?\d{1,9}$/.test(
-    str
+    str,
   );
 
 export async function action({ request }) {
@@ -14,7 +18,7 @@ export async function action({ request }) {
   const order = {
     ...data,
     cart: JSON.parse(data.cart),
-    priority: data.priority === "on",
+    priority: data.priority === "true",
   };
 
   const errors = {};
@@ -24,6 +28,8 @@ export async function action({ request }) {
   if (Object.keys(errors).length > 0) return errors;
 
   const newOrder = await createOrder(order);
+
+  store.dispatch(clearCart());
 
   return redirect(`/order/${newOrder.id}`);
 }
