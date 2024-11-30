975270cc063e67072185801f7dc31431a3c2e409
Xavi Enriquez <113799913+XavierEnriquez@users.noreply.github.com>
2024-11-29 23:54:51 -0600

    Update CreateOrder.jsx

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,54 +1,61 @@
 // import { useState } from "react";
 import { Form, useActionData, useNavigation } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { useState } from "react";
 
+import { formatCurrency } from "../../utils/helpers";
+import { fetchAddress } from "../user/userSlice";
+import { getCart, getCartTotal } from "../cart/cartSlice";
 import Button from "../../ui/Button";
 import Input from "../../ui/input";
-
-const fakeCart = [
-  {
-    pizzaId: 12,
-    name: "Mediterranean",
-    quantity: 2,
-    unitPrice: 16,
-    totalPrice: 32,
-  },
-  {
-    pizzaId: 6,
-    name: "Vegetale",
-    quantity: 1,
-    unitPrice: 13,
-    totalPrice: 13,
-  },
-  {
-    pizzaId: 11,
-    name: "Spinach and Mushroom",
-    quantity: 1,
-    unitPrice: 15,
-    totalPrice: 15,
-  },
-];
+import EmptyCart from "../cart/EmptyCart";
 
 function CreateOrder() {
-  const username = useSelector((state) => state.user.username);
+  const dispatch = useDispatch();
+  const formErrors = useActionData();
+
+  const [withPriority, setWithPriority] = useState(false);
+
   const navigation = useNavigation();
   const isSubmitting = navigation.state === "submitting";
 
-  const formErrors = useActionData();
+  const {
+    username,
+    status: addressStatus,
+    position,
+    address,
+  } = useSelector((state) => state.user);
 
-  // const [withPriority, setWithPriority] = useState(false);
-  const cart = fakeCart;
+  const isLoadingAddress = addressStatus === "loading";
+
+  const cart = useSelector(getCart);
+  const subTotal = useSelector(getCartTotal);
+  const priorityFee = withPriority ? subTotal * 0.2 : 0;
+  const orderTotal = subTotal + priorityFee;
+
+  if (!cart.length) return <EmptyCart />;
 
   return (
     <div className="m-auto mt-[5vmin] flex max-w-xl flex-col gap-8">
-      <h2 className="text-2xl font-semibold">Ready to order? Let&apos;s go!</h2>
+      <div>
+        <Button type="link" to="/cart" className="link">
+          &larr; Modify your order
+        </Button>
+      </div>
+      <h2 className="text-2xl font-semibold">
+        To complete your order, fill out the form and click the button below!
+      </h2>
 
       <Form method="POST" className="flex flex-col gap-6">
         <div className="flex flex-wrap gap-2 sm:flex-row sm:flex-nowrap sm:items-center">
-          <label className="sm:basis-36">Name</label>
+          <label htmlFor="customer" className="sm:basis-36">
+            Name
+          </label>
           <Input
+            className="input"
             type="text"
             name="customer"
+            id="customer"
             defaultValue={username}
             required={true}
           />
@@ -56,8 +63,16 @@ function CreateOrder() {
 
         <div className="flex flex-col">
           <div className="flex flex-col gap-2 sm:flex-row sm:items-center">
-            <label className="sm:basis-36">Phone number</label>
-            <Input type="tel" name="phone" required={true} />
+            <label htmlFor="phone" className="sm:basis-36">
+              Phone number
+            </label>
+            <Input
+              className="input"
+              type="tel"
+              id="phone"
+              name="phone"
+              required={true}
+            />
           </div>
           <div className="justify-items-center sm:justify-items-end">
             {formErrors?.phone && (
@@ -66,28 +81,75 @@ function CreateOrder() {
           </div>
         </div>
 
-        <div className="flex flex-col gap-2 sm:flex-row sm:items-center">
-          <label className="sm:basis-36">Address</label>
-          <Input type="text" name="address" required={true} />
-        </div>
+        <div className="flex flex-col gap-2">
+          <div className="flex items-center justify-end gap-4 text-sm">
+            {!position.latitde && !position.longitude ? (
+              <>
+                <p className="italic">
+                  Share your location or type address below
+                </p>
+                <Button
+                  className="small"
+                  disabled={isLoadingAddress}
+                  onClick={(e) => {
+                    e.preventDefault();
+                    dispatch(fetchAddress());
+                  }}
+                >
+                  {isLoadingAddress ? "Loading..." : " Share location"}
+                </Button>
+              </>
+            ) : (
+              <p className="italic">Make sure the address below is correct</p>
+            )}
+          </div>
 
+          <div className="flex flex-col gap-2 sm:flex-row sm:items-center">
+            <label htmlFor="address" className="sm:basis-36">
+              Address
+            </label>
+            <Input
+              className="input"
+              type="text"
+              name="address"
+              id="address"
+              defaultValue={address}
+              required={true}
+              disabled={isLoadingAddress}
+            />
+          </div>
+        </div>
         <div className="mt-4 flex gap-4 align-middle">
-          <input
-            className="h-6 w-6 accent-yellow-400 focus:outline-none focus:ring focus:ring-yellow-400 focus:ring-offset-2"
+          <Input
+            className="checkbox"
             type="checkbox"
             name="priority"
-            id="priority"
-            // value={withPriority}
-            // onChange={(e) => setWithPriority(e.target.checked)}
+            id="order_priority"
+            value={withPriority}
+            onChange={(e) => setWithPriority(e.target.checked)}
           />
-          <label htmlFor="priority" className="font-medium">
-            Want to yo give your order priority?
+          <label htmlFor="order_priority" className="font-medium">
+            Make my order priority!
           </label>
         </div>
-        <div className="flex justify-end pt-2">
-          <Button className="primary" disabled={isSubmitting}>
-            {isSubmitting ? "Placing order..." : "Order now"}
+
+        <div className="flex flex-wrap-reverse items-start justify-between gap-8 pt-4">
+          <Button
+            className="primary"
+            disabled={isSubmitting || isLoadingAddress}
+          >
+            {isSubmitting ? "Placing order..." : "Place order now"}
           </Button>
+          <div className="flex flex-col gap-2 rounded-lg border border-stone-300 bg-stone-200 px-8 py-4 text-right text-lg text-stone-700">
+            <p>
+              Sub-total:{" "}
+              <span className="font-semibold"> {formatCurrency(subTotal)}</span>
+            </p>
+            <p>
+              Total:{" "}
+              <span className="font-bold"> {formatCurrency(orderTotal)}</span>
+            </p>
+          </div>
         </div>
         <input type="hidden" name="cart" value={JSON.stringify(cart)} />
       </Form>
