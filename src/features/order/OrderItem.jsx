aa32a44fbc33e6fc069fd00ac0bfaf4f39419ec3
Xavi Enriquez <113799913+XavierEnriquez@users.noreply.github.com>
2024-11-30 14:00:14 -0600

    Update OrderItem.jsx

--- src/features/order/OrderItem.jsx
+++ src/features/order/OrderItem.jsx
@@ -5,12 +5,15 @@ function OrderItem({ item, isLoadingIngredients, ingredients }) {
 
   return (
     <li className="py-3">
-      <div className="flex flex-wrap items-center justify-between">
+      <div className="mb-1 flex flex-wrap items-center justify-between">
         <p className="basis-60">
           <span className="font-semibold">{quantity}&times;</span> {name}
         </p>
         <p className="font-semibold">{formatCurrency(totalPrice)}</p>
       </div>
+      <p className="text-sm capitalize italic text-stone-500">
+        {isLoadingIngredients ? "Loading..." : ingredients.join(", ")}
+      </p>
     </li>
   );
 }
